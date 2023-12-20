a9070d252ffe1531f1fe514c405a919ee7c372ce
antoscardi <antonio.scard97@gmail.com>
2023-12-20 11:20:44 +0100

    finito dataset

--- include/dataset.py
+++ include/dataset.py
@@ -1,11 +1,12 @@
-from pickle import load
 import cv2
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import json
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
-from utility import*
-import json
+from torch.utils.data import DataLoader, Dataset
+from utility import *
+
 # alias
 from os.path import join as join_paths
 
@@ -22,7 +23,7 @@ def write_file(output_path, data):
     # Writes a list of dictionaries to a JSON file.
     with open(output_path, 'w') as file:
         json.dump(data, file, indent=2)  # indent for pretty formatting (optional)
-  
+
     return 
 
 def match_in_path(path, expression):
@@ -34,7 +35,7 @@ def match_in_path(path, expression):
     return number
 
 '''
-WRAPPER FORD DATASET CLASS 
+WRAPPER FOR DATASET CLASS 
 '''
 class DataLoaderVisualizer:
     def __init__(self, root, file_path, percentage,  split='train'):
@@ -95,73 +96,76 @@ class DataLoaderVisualizer:
                 sample_path = join_paths(driver_view_path, driver_sample)
                 driver_info["driver_view"][driver_sample] = sample_path   
             path_structure[driver] = driver_info
-        #import pprint
-        #pprint.pprint(path_structure)
+        # import pprint
+        # pprint.pprint(path_structure)
         return path_structure
     
     def load_data(self, percentage):
-      data = []
-      for driver, views in self.path_structure.items():
-          driver_view_samples = views['driver_view']
-          data_item = {}
-          data_item_complete = {}
+        data = []
+        for driver, views in self.path_structure.items():
+            driver_view_samples = views['driver_view']
+            data_item = {}
+            data_item_complete = {}
           
-          for sample_name, sample_path in driver_view_samples.items():
-              video_images = [img for img in os.listdir(sample_path)]
+            for sample_name, sample_path in driver_view_samples.items():
+                video_images = [img for img in os.listdir(sample_path)]
 
-              # Determine the number of images to load based on the specified percentage
-              num_images_to_load = int(len(video_images) * (percentage / 100))
-              selected_images = random.sample(video_images, num_images_to_load)
+                # Determine the number of images to load based on the specified percentage
+                num_images_to_load = int(len(video_images) * (percentage / 100))
+                selected_images = random.sample(video_images, num_images_to_load)
             
-              
-              for img in  selected_images:
-                  # Save the image path
-                  data_item['path'] = join_paths(sample_path, img)
-                  data_item_complete[' driver path'] = join_paths(sample_path, img)
-                  # Save road view path
-                  data_item_complete[' road view path'] = join_paths(sample_path.replace('driver_view', 'road_view'), img)
+                for img in  selected_images:
+                    # Save the image path
+                    data_item['path'] = join_paths(sample_path, img)
+                    data_item_complete[' driver path'] = join_paths(sample_path, img)
+                    # Save road view path
+                    data_item_complete[' road view path'] = join_paths(sample_path.replace('driver_view', 'road_view'), img)
 
-                  # Get frame number
-                  frame_number = int(img.split("_")[1].split(".")[0])
-                  gt_path = sample_path.replace('driver_view', 'road_view') + '.npy'
-                  gt_array = np.load(gt_path, allow_pickle=True)
-                  sample_number = match_in_path(gt_path, sample_name)
-                  frame_number_label, gaze_point, boundbox = get_frame_and_point(gt_array, frame_number, sample_number)      
-                  assert frame_number == frame_number_label, f"Not the same frame number: frame_number={frame_number}, " \
+                    # Get frame number
+                    frame_number = int(img.split("_")[1].split(".")[0])
+                    gt_path = sample_path.replace('driver_view', 'road_view') + '.npy'
+                    gt_array = np.load(gt_path, allow_pickle=True)
+                    sample_number = match_in_path(gt_path, sample_name)
+                    frame_number_label, gaze_point, bbox = get_frame_and_point(gt_array, frame_number, sample_number)      
+                    assert frame_number == frame_number_label, f"Not the same frame number: frame_number={frame_number}, " \
                                                               f"frame_number_label={frame_number_label}, img_path ={img}, " \
                                                               f"array={gt_path}" 
-                  # Save Lavel
-                  data_item['label']  = list(gaze_point)
-                  data_item_complete['label']  = list(gaze_point)             
-      return data
-
+                    # Save Label
+                    data_item['label']  = list(gaze_point)
+                    data_item_complete['label']  = list(gaze_point)
+                    data_item_complete['bbox']  = list(bbox)  
+                    # Append data items to the list
+                    data.append(data_item)
+                    self.data_complete.append(data_item_complete)
+        return data
 
     def _visualize_dataset(self):
         for driver,_ in self.path_structure.items():
           driver_x_paths = find_driver_paths(self.data_complete['paths'], driver)
-          driver_img = random.sanple(driver_x_paths)
-          road_image = driver_img
-
-        
-        road_photo = cv2.imread(road_photo_path)
-        road_photo = cv2.cvtColor(road_photo, cv2.COLOR_BGR2RGB)
-
-        # Bounding box from .npy files
-        rect = patches.Rectangle((boundbox[0], boundbox[1]), boundbox[2]-boundbox[0], boundbox[3]-boundbox[1],
+          driver_img_path = random.sample(driver_x_paths)
+          road_img_path = driver_img_path.replace('driver_view', 'road_view')
+          # Load Images
+          driver_photo = cv2.imread(driver_img_path)
+          driver_photo = cv2.cvtColor(driver_photo, cv2.COLOR_BGR2RGB)
+          road_photo = cv2.imread(road_img_path)
+          road_photo = cv2.cvtColor(road_photo, cv2.COLOR_BGR2RGB)
+          # Get the bounding box
+          matching_item = next((item for item in self.data_complete if item['path'] == driver_img_path), None)
+          boundbox = matching_item['bbox']
+          rect = patches.Rectangle((boundbox[0], boundbox[1]), boundbox[2]-boundbox[0], boundbox[3]-boundbox[1],
                                  linewidth=1, edgecolor='r', facecolor='none')
-
-        # Display
-        fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(11, 5))
-        ax1.imshow(driver_photo)
-        ax1.set_title(f'{driver} view photo'), ax1.set_xlabel('W'), ax1.set_ylabel('H')
-
-        ax2.imshow(road_photo)
-        ax2.scatter(gaze[0], gaze[1], color='red', marker='x', s=100)
-        ax2.add_patch(rect)
-        ax2.set_title('road view photo'), ax2.set_xlabel('W'), ax2.set_ylabel('H')
-        ax2.set_box_aspect(driver_photo.shape[0] / driver_photo.shape[1])
-
-        plt.show()
+          # Get the gaze point
+          gaze = matching_item['label']
+          # Display
+          fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(11, 5))
+          ax1.imshow(driver_photo)
+          ax1.set_title(f'{driver} view photo'), ax1.set_xlabel('W'), ax1.set_ylabel('H')
+          ax2.imshow(road_photo)
+          ax2.scatter(gaze[0], gaze[1], color='red', marker='x', s=100)
+          ax2.add_patch(rect)
+          ax2.set_title('road view photo'), ax2.set_xlabel('W'), ax2.set_ylabel('H')
+          ax2.set_box_aspect(driver_photo.shape[0] / driver_photo.shape[1])
+          plt.show()
 
 '''
 PYTORCH DATASET CLASS 
@@ -169,7 +173,7 @@ PYTORCH DATASET CLASS
 class DGAZEDataset(Dataset):
     def __init__(self, loader_class, split='train', save_file='train_paths.json'):
         self.split = split
-        
+
         if match_in_path(save_file, split):
             self.save_file = save_file
         else:
